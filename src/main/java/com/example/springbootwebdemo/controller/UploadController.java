d8ad2c0c54343682a95370b8a5af4dae2a13d5f1
smileshy0104 <1259581033@qq.com>
2024-06-16 13:36:34 +0800

    上传文件oss

--- src/main/java/com/example/springbootwebdemo/controller/UploadController.java
+++ src/main/java/com/example/springbootwebdemo/controller/UploadController.java
@@ -1,7 +1,9 @@
 package com.example.springbootwebdemo.controller;
 
 import com.example.springbootwebdemo.pojo.Result;
+import com.example.springbootwebdemo.utils.AliOSSUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +38,13 @@ public class UploadController {
         return Result.success();
     }
 
+    @Autowired
+    private AliOSSUtils aliOSSUtils;
+    @PostMapping("/upload_oss")
+    public Result uploadOss(MultipartFile image) throws IOException {
+        //调用阿里云OSS工具类，将上传上来的文件存入阿里云
+        String url = aliOSSUtils.upload(image);
+        //将图片上传完成后的url返回，用于浏览器回显展示
+        return Result.success(url);
+    }
 }
