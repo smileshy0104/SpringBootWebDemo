497d2e0b3c1578d3cd4417bd69ee45087acc208b
smileshy0104 <1259581033@qq.com>
2024-06-16 14:08:50 +0800

    debug

--- src/main/java/com/example/springbootwebdemo/utils/AliOSSUtils.java
+++ src/main/java/com/example/springbootwebdemo/utils/AliOSSUtils.java
@@ -11,31 +11,11 @@ import java.util.UUID;
 
 @Component
 public class AliOSSUtils {
-    private String endpoint = "11111111111";
-    private String accessKeyId = "11111111111";
-    private String accessKeySecret = "1111111111111";
-    private String bucketName = "11111111111111111";
 
     /**
      * 实现上传图片到OSS
      */
     public String upload(MultipartFile multipartFile) throws IOException {
-        // 获取上传的文件的输入流
-        InputStream inputStream = multipartFile.getInputStream();
-
-        // 避免文件覆盖
-        String originalFilename = multipartFile.getOriginalFilename();
-        String fileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
-
-        //上传文件到 OSS
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        ossClient.putObject(bucketName, fileName, inputStream);
-
-        //文件访问路径
-        String url = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + fileName;
-
-        // 关闭ossClient
-        ossClient.shutdown();
-        return url;// 把上传到oss的路径返回
+      return "111111";
     }
 }
